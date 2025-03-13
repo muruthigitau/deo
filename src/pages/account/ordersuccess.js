898f46e02ce8a9855118ddcb4660071038f12977
kimash-255 <rexviscot44@gmail.com>
2025-03-13 23:58:34 +0300

    order-sucess

---
+++ src/pages/account/ordersuccess.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "@/components/layout";
+import OrderSuccess from "@/components/account/ordersuccess";
+
+const OrderSuccessPage = () => {
+  return (
+    <Layout>
+      <OrderSuccess />
+    </Layout>
+  );
+};
+export default OrderSuccessPage;
